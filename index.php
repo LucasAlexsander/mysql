3392188782c60947de249caf16fe511cc0695db1
lucasalexsander <lucas.alexsander@ufvjm.edu.br>
2022-01-03 12:52:01 -0300

    Firt commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>mySQL Documents</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <div class="ponto"></div>
+        </div>
+        <nav>
+            <ul>
+                <li>Temas:&emsp;</li>
+                <li><a href="#">Exemp1</a></li>
+                <li><a href="#">Exemp2</a></li>
+                <li><a href="#">Exemp3</a></li>
+                <li><a href="#">Exemp4</a></li>
+                <li><a href="#">Exemp5</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="line-space"></div>
+
+    <section>
+        <div class="container">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at purus eget mi porttitor ultrices. Praesent quis leo sit amet augue egestas pellentesque. Quisque ac libero hendrerit, malesuada purus sit amet, rutrum tellus. Etiam elementum quam mauris, pretium malesuada neque dignissim eu. Donec augue justo, laoreet eu maximus eu, fringilla non turpis. Morbi enim nunc, laoreet in quam gravida, interdum finibus lacus. Fusce tincidunt orci sit amet sollicitudin gravida. Phasellus euismod, nibh vel facilisis varius, est nibh sollicitudin sem, ac accumsan orci lectus in lorem. Sed at sapien ligula. Nunc a enim dolor. Aenean bibendum auctor mauris id feugiat. Nulla iaculis dapibus elit. Donec a facilisis turpis. Proin commodo tellus vitae mauris molestie, at finibus enim commodo. Proin semper lectus sed urna ornare, in ornare lorem faucibus. Vestibulum dapibus condimentum felis, nec consectetur arcu euismod tristique.
+
+            Sed lorem neque, lacinia at convallis quis, finibus vitae ex. Morbi posuere, sapien id tincidunt ultricies, lorem lectus condimentum mauris, non ullamcorper elit sapien nec est. Duis ipsum ipsum, viverra id quam eu, vulputate ullamcorper eros. Nunc vitae mauris sagittis, vehicula tellus id, rhoncus lacus. Phasellus luctus ex eget leo interdum consectetur. Quisque et nunc tristique, gravida felis volutpat, auctor magna. Ut sit amet lectus arcu. Aliquam suscipit erat felis, in sagittis erat ullamcorper eu. Suspendisse potenti. Nulla eget odio nec lacus venenatis placerat. Fusce rutrum diam at enim interdum, ac fringilla orci mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nisi turpis, porttitor nec nisi et, faucibus ornare leo. Maecenas et gravida sapien. Nunc nibh elit, luctus sit amet gravida id, dictum ac sem.
+            </p>
+        </div>
+    </section>
+</body>
+</html>
